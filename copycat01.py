b0b1e5396a0357e5a790c26e7f1f8e38a38b236c
williafw <angel1que2003@yahoo.com>
2018-12-11 11:33:21 -0500

    Lab 8

---
+++ copycat01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python::3
+import shutil
+import os
+os.chdir('/home/student/mycode/')
+shutil.copy('5g_research/sdn_network.txt', '5g_research/sdn_network.txt.copy')
+shutil.copytree('5g_research/', '5g_research_backup/')
