6cd28aa86002eec9756828a3a875f8124d5897fa
williafw <angel1que2003@yahoo.com>
2018-12-11 13:39:52 -0500

    Lab 9

---
+++ test_001_integers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# This is a print message.  It printss out the text, "Hello, there!"
+print('Hello, there')
+
+# Initialize a variable, a is a simple integer
+a=2
+
+# This should print out the integer 2
+print(a)
+print("...Check that it printed out 2")
+print()  # This prints an emply line
+
+# Initialize another variable, b is a floating point integer
+b=2.45
+
+# This should print the floating point integer
+print(b)
+print("...Check that it printed out 2.45")
+
+# Intitialize another variable, c is a string
+c='my string'
+
+# This should print out the string
+print(c)
+print("...Check that it printed out'my string'")
+
+# Here is another way to print an empty line
+print("\n")  #This prints an empty line also
