18bc24192fd53cc6a31251c45f5968f8e11d4526
williafw <angel1que2003@yahoo.com>
2018-12-13 13:34:48 -0500

    LAB 15

---
+++ custif/cust_if_script.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#LAB 15 
+
+message = 'Your Grade is '
+print('Grading has been completed ')
+Grade = float(input())
+if Grade >= 100-90:
+    message = message + 'A 100-90.'
+elif Grade >= 89-80:
+    message = message + 'B.'
+elif Grade >= 79-70:
+    message = message + 'C.'
+elif Grade >= 69-60:
+    message = message + 'D.'
+elif Grade <= 59:
+    message = message + 'F.'
+else:
+    message = message + 'incomplete.'
+print(message)
+
+
+message = 'The movie is about to begin, we recommend '
+print('What is your connection speed in Mbps?')
+connection = float(input())
+if connection >= 25:
+    message = message + 'setting video to 4k.'
+elif connection >= 5:
+    message = message + 'setting video to 1080p.'
+elif connection >= 2:
+    message = message + 'setting video to 720p.'
+else:
+    message = message + 'finding another access network.'
+print(message)
+
+
+
